e49408a194fdae9da2791a9fce556ae18a6a6c02
byy <2714540821@qq.com>
2017-12-14 12:49:51 +0800

    a test

---
+++ src/com/test3/Light.java
@@ -0,0 +1,24 @@
+package com.test3;
+
+public class Light {
+    public void On() {
+
+    }
+
+    public void Off() {
+
+    }
+
+    public void brighten() {
+
+    }
+
+    public void dim() {
+
+    }
+
+    public static void main(String[] args) {
+        Light it=new Light();
+        it.On() ;  
+    }
+}
